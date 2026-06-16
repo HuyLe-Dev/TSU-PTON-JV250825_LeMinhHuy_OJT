01755fea02031abb7f3e687724f7f13e911a7acb
huyDev <dev.minhuy@gmail.com>
2026-06-16 20:59:40 +0900

    project init

---
+++ smart_cinema_booking_system/src/main/java/com/example/smart_cinema_booking_system/SmartCinemaBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_cinema_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCinemaBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCinemaBookingSystemApplication.class, args);
+	}
+
+}
