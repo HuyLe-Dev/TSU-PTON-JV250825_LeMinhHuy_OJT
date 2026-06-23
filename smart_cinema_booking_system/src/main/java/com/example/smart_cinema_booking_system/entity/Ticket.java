653dbcd32a543b9a3d87f65ae7bad4c5f773e4e6
huyDev <dev.minhuy@gmail.com>
2026-06-23 22:45:45 +0900

    Merge branch 'refactor/add-seat-and-ticket-table-for-3NF'

---
+++ smart_cinema_booking_system/src/main/java/com/example/smart_cinema_booking_system/entity/Ticket.java
@@ -0,0 +1,40 @@
+package com.example.smart_cinema_booking_system.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tickets", uniqueConstraints = {
+    @UniqueConstraint(columnNames = { "showtime_id", "seat_id" })
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ticket_id")
+    private Long ticketId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "showtime_id", nullable = false)
+    private Showtime showtime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seat_id", nullable = false)
+    private Seat seat;
+
+    @Column(name = "price", precision = 10, scale = 2, nullable = false)
+    private BigDecimal price;
+}
